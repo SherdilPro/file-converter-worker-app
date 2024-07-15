0ef3e4dad5daea562a267949f446489d39abfb2d
Muhammd Bilal <Sherdilpro@gmail.com>
2024-07-15 15:56:36 +0500

    initial

---
+++ src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import * as XLSX from 'xlsx';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'file-converter-worker-app - xlsx-to-csv';
+  onFileChange(evt: any) {
+    const target: DataTransfer = <DataTransfer>(evt.target);
+    if (target.files.length !== 1) throw new Error('Cannot use multiple files');
+
+    const reader: FileReader = new FileReader();
+    reader.onload = (e: any) => {
+      const bstr: string = e.target.result;
+      const wb: XLSX.WorkBook = XLSX.read(bstr, { type: 'binary' });
+
+      const wsname: string = wb.SheetNames[0];
+      const ws: XLSX.WorkSheet = wb.Sheets[wsname];
+
+      const csvData: string = XLSX.utils.sheet_to_csv(ws);
+      this.downloadCSV(csvData, 'converted.csv');
+    };
+    reader.readAsBinaryString(target.files[0]);
+  }
+
+  downloadCSV(csv: string, filename: string) {
+    const blob: Blob = new Blob([csv], { type: 'text/csv' });
+    const url: string = window.URL.createObjectURL(blob);
+    const a: HTMLAnchorElement = document.createElement('a');
+    a.href = url;
+    a.download = filename;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+  }
+}
